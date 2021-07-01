8d85e8b86e90a09b588edfafd144701bacb00e61
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-07-01 16:30:02 +0600

    01.07.2021

--- test.php
+++ test.php
@@ -1,29 +1,7 @@
 <?php
-
-	class CloneableUser
-		{
-		    public $name;
-		    public $lastName;
-
-		    /**
-		     * This method will be invoked by a clone operator and will prepend "Copy " to the
-		     * name and lastName properties.
-		     */
-		    public function __clone()
-		    {
-		    	echo "string";
-		        // $this->name = "Copy " . $this->name;
-		        // $this->lastName = "Copy " . $this->lastName;
-		    }
-		}
-
-		$x = new CloneableUser();
-	clone $x;
-//$user1->name = "John";
-//$user1->lastName = "Doe";
-
-// $user2 = clone $user1; // triggers the __clone magic method
-
-// echo $user2->name;     // Copy John
-// echo $user2->lastName; // Copy Doe
+	$x = [10,12,13];
+	print_r($x);
+	array_shift($x);
+	print "<br/>";
+	print_r($x);
 
