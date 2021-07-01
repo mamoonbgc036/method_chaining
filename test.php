2c3b39db512b1fad4ea605c3f02b5d23c88c2208
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-07-01 12:02:18 +0600

    01.07.2021

--- test.php
+++ test.php
@@ -15,7 +15,16 @@
 	// }
 
 	$x = new Test();
-	$y = clone $x;
+	$y = $x;
+	$z = clone $x;
 	$y->name = "noman";
+	$z->name = "sharif";
 
+	// here variable x and y point to same object that why if there is change in object y, it will affect object x as well
 	echo $x->name;
+	echo "<br/>";
+
+	// here as z is a clone object of Test class,it create another copy of Test class in the memory,so any change in z object will not affect in y and x variable
+	echo $z->name;
+	echo "<br/>";
+	echo $y->name;
