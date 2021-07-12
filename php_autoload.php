d50d4d867c3dd068f26824ea8fee9fe7be8e958c
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-07-12 12:44:01 +0600

    12.07.2021

--- php_autoload.php
+++ php_autoload.php
@@ -11,3 +11,12 @@
 	spl_autoload_register("App\my_autoload");
 
 	new show();
+
+	// use App\auto\load\show;
+	// new show();
+
+	// in above case, autoload will have App\auto\load\show.php class irrespective of haveing namespace;
+
+	// new App\auto\load\show();
+
+	// in above case, autoload will have App\App\auto\load\show.php in present file.
