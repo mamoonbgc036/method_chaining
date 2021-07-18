5ad9b518a3c4bb3c028c34241b349ae8be876d05
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-07-18 12:57:09 +0600

    18.07.2021

--- test.php
+++ test.php
@@ -1,7 +1,29 @@
 <?php
 
- $subname = "mamoon";
- $x = $subname ?? "noman";
- 
- echo $x;
+ class Test{
+	public $name;
+	public $eligible = false;
+	public static $instance;
+
+	public static function getInstance(){
+		if (!isset($instance)){
+			static::$instance = new Test();
+		}
+		return static::$instance;
+	}
+
+ 	public function get_Student($name){
+ 		$this->name=$name;
+ 		return $this;
+ 	}
+
+ 	public function isAbove35($age){
+ 		if ($age > 35){
+ 			$this->eligible = true;
+ 		}
+ 		return $this->eligible;
+ 	}
+ }
+
+ Test::getInstance()->get_Student("mamoon")->isAbove35(36);
 	
