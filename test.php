8b3ea6f03fe228427515e35b2ddc44227038b603
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-08-05 10:21:28 +0600

    05.08.2021

--- test.php
+++ test.php
@@ -1,27 +1,9 @@
 <?php
 
-$num = 121;
-
-switch($num){
-	case 10:
-	echo "i am ten";
-	break;
-	case 11:
-	echo "i am eleven";
-	break;
-	default:
-	echo "i am not number";
-	break;
-	case 12:
-	echo "i am twelve";
-	break;
-}
-
-for ($i=0; $i < 10 ; $i++) { 
-	for ($j=0; $j < 10 ; $j++) { 
-		if( $i == $j ){
-			echo "i is ".$i."\n";
-			echo "j is ".$j."\n";
-		}
-	}
-}
+$filename = "test.txt";    
+$handle = fopen($filename, "r");//open file in read mode    
+  
+$contents = fread($handle, filesize($filename));//read file    
+  
+echo $contents;//printing data of file  
+fclose($handle);//close file   
