427d95590f388d543be3dfd6ae882d5ca95bdfda
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-08-07 16:21:44 +0600

    07.08.2021

--- test.php
+++ test.php
@@ -1,8 +1,23 @@
 <?php
 
-$fp = fopen('test.txt', 'a');//opens file in append mode  
-fwrite($fp, ' this is additional text ');  
-fwrite($fp, 'appending data');  
-fclose($fp);  
-  
-echo "File appended successfully";   
+echo __FILE__;
+echo "\n";
+echo __DIR__;
+echo "\n";
+
+function name(){
+	return __FUNCTION__;
+}
+
+echo name();
+
+echo "\n";
+
+class Test{
+	public function getClassName(){
+		return __CLASS__;
+	}
+}
+
+$x = new Test();
+echo $x->getClassName();
