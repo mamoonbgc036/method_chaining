a4175f4134ff62ec10debdb0a79d944d7830defa
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-08-10 13:02:11 +0600

    10.08.2021

--- test.php
+++ test.php
@@ -1,4 +1,36 @@
-<?php
+<?php  
+ class Str
+{
+  private $s = '';
 
- echo $_SERVER['HTTP_HOST'];
+  private $functions = [
+    'length' => 'strlen',
+    'upper' => 'strtoupper',
+    'lower' => 'strtolower'
+    // map more method to functions
+  ];
 
+  public function __construct(string $s)
+  {
+    $this->s = $s;
+  }
+
+  public function __call($method, $args)
+  {
+    if (!in_array($method, array_keys($this->functions))) {
+      throw new BadMethodCallException();
+    }
+
+    array_unshift($args, $this->s);
+
+    return call_user_func_array($this->functions[$method], $args);
+  }
+}
+
+$string = "Hello,World";
+
+$s = new Str($string);
+
+echo $s->upper() . '<br>'; // HELLO, WORLD!
+echo $s->lower() . '<br>'; // hello, world!
+echo $s->length() . '<br>'; 
