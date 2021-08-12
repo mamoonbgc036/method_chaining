2862cbcfe79660ec2452bdb8043440148ab2ec3a
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-08-12 17:34:48 +0600

    12.08.2021

--- test.php
+++ test.php
@@ -6,11 +6,12 @@
             print "Woof!\n";
         }
 
-        // public function meow() {
-            // print "Dogs don't meow!\n";
-        // }
+        public function meow() {
+            print "ok Dogs don't meow!\n";
+        }
 
         public function __call($function, $args) {
+          echo "string";die();
             $args = implode(', ', $args);
             print "Call to $function() with args '$args' failed!\n";
         }
